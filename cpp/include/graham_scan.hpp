b25cc77688f42937a7e5821f9d6b96743f16e1d0
Sude <151455137+SudeErzurumlu@users.noreply.github.com>
2024-10-27 12:43:41 +0300

    Create graham_scan.hpp

---
+++ cpp/include/graham_scan.hpp
@@ -0,0 +1,9 @@
+#ifndef GRAHAM_SCAN_HPP
+#define GRAHAM_SCAN_HPP
+
+#include "point.hpp"
+#include <vector>
+
+std::vector<Point> grahamScan(std::vector<Point>& points);
+
+#endif // GRAHAM_SCAN_HPP
